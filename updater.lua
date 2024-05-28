26a165b3b41445f4aea52b8308b198cf8e3ba587
frezee <135590800+frzzee@users.noreply.github.com>
2024-05-29 00:05:20 +0700

    updater.lua

--- updater.lua
+++ updater.lua
@@ -11,6 +11,7 @@ patern = {
     baseimg = 0x20000000,
     portals = 0x2000000,
     playerbase = 0x00,
+    strangerx = 0x1FFFFF,
     baseptr = 0x14D5,
     f2k = 0x300000,
     f5ty = 0x50FFFFF,
